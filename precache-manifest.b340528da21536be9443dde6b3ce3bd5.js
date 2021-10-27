ae894396eebc13d2d1d41c5b1f6f1385a7dcd92a
jjcaballero <jjcaballero@users.noreply.github.com>
2021-10-27 14:48:02 +0000

    deploy: 17afcad49d05dfbb25a2629b558ffbfdcf135ed5

---
+++ precache-manifest.b340528da21536be9443dde6b3ce3bd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c1dff28ace52c6777959f0d04f9653",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "846ef394b8cb1409b760",
+    "url": "/react-table/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7de7628084e07b4904bf",
+    "url": "/react-table/static/js/2.8c574aec.chunk.js"
+  },
+  {
+    "revision": "846ef394b8cb1409b760",
+    "url": "/react-table/static/js/main.8fd0adf4.chunk.js"
+  },
+  {
+    "revision": "dad51b5cb92572d43e00",
+    "url": "/react-table/static/js/runtime~main.15ef8938.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2dc0b7449ad5972107d5974400cfcbe.js"
+  "/react-table/precache-manifest.b340528da21536be9443dde6b3ce3bd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-table/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
